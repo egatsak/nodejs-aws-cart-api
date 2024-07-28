b8ee53fe1cb6b15188f617827cfe39b538f88b96
Eugene Gatsak <greenglaz@gmail.com>
2024-07-28 22:42:13 +0200

    chore: delete unused comments

--- src/main.ts
+++ src/main.ts
@@ -4,46 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import 'dotenv/config';
 
-/* 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-
-  app.use(helmet());
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-
-  return serverlessExpress({
-    app: expressApp,
-  });
-} */
-
-/* export const handler: Handler = async (
-  event: unknown,
-  context: Context,
-  callback: Callback,
-) => {
-  console.log(`Bootstrap started...`);
-  if (!server) {
-    server = await bootstrap();
-  }
-  console.log(`Bootstrap finished!`);
-  console.log(`Event: ${JSON.stringify(event)}`);
-  return server(event, context, callback);
-}; */
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
